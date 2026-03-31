1029619db145baa63f3c595b0868465378df4afd
João Victor <j.victor.baros.live@gmail.com>
2026-03-30 21:39:53 -0300

    criando o models e suas migrações

--- mural/admin.py
+++ mural/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Categoria, Aviso
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ('nome',)
+
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
+    # Exibição das colunas na listagem
+    list_display = ('titulo', 'caegoria', 'data_criacao')
+    
+    # Filtros laterais para facilitar a navegação
+    list_filter = ('caegoria', 'data_criacao')
+    
+    # Barra de pesquisa para títulos
+    search_fields = ('titulo', 'conteudo')
+    
+    # Ordenação padrão (mais recentes primeiro)
+    ordering = ('-data_criacao',)
